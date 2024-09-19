6841757cb931e23abfd40e9c3fb3f44b7b4cfa4b
tommyallen <thomasclansi228@gmail.com>
2024-09-19 15:31:24 +0700

    Add cycle "for", and function to solve result

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
 import android.widget.Toast
+import java.lang.Math.pow
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +21,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
 
     val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
     val editTextNumber2 = findViewById<EditText>(R.id.editTextNumber2)
     val buttonCalculate = findViewById<Button>(R.id.button)
 
+    buttonCalculate.setOnClickListener{
+        val x = editTextNumber.text.toString().toDoubleOrNull()
+        val n = editTextNumber2.text.toString().toIntOrNull()
+        var i: Int = 1
+        var res:Double = 0.0
 
+        if (x != null && n != null)
+        {
+            for (i in 1..n)
+            {
+                res = res + (x/(pow(4.0, i.toDouble())+pow(5.0,i.toDouble()+2.0)))
+            }
+            Toast.makeText(this, "Результат: $res", Toast.LENGTH_LONG).show()
+
+        }
+        else{
+
+        }
+
+
+        }
+    }
 }
